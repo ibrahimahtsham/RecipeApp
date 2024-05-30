ffd03739da37ebe3b9c6a313e8d01cd8c94e1cf1
Siamax <ibrahimahtsham2002@gmail.com>
2024-05-30 19:53:52 +0500

    icons and color scheme changed (tried to atleast)

--- app/src/main/java/com/siamax/recipeapp/ui/theme/Theme.kt
+++ app/src/main/java/com/siamax/recipeapp/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.siamax.recipeapp.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -12,15 +11,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = Orange80,
+    secondary = Green80,
+    tertiary = OrangeGrey80
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = Orange40,
+    secondary = Green40,
+    tertiary = OrangeGrey40
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
